8fc0e1470f3a3b7faa638952336fce444fd8a6fc
PolySwanFinance <polyswanfinance@gmail.com>
2021-05-30 21:04:51 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# PolySwan Frontend
+
+This project contains the main features of the PolySwan application.
---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,31 @@
+export default {
+  cake: {
+    137: '0x692ca411d82DeB4A96F8f89E173171b4Af83Badf'
+  },
+  masterChef: {
+    137: '0x126a1D2293943eb69056E8063Ded39Aa64453b03',
+  },
+  weth: {
+    137: "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619"
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    137: '0x40524425bcb58Dc16f11746B0028e23aCDA1e128'
+  },
+  usdc: {
+    137: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
+  },
+  wmatic: {
+    137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+  },
+  matic: {
+    137: '0x0000000000000000000000000000000000001010'
+  }
+}
