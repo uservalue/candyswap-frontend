639e188f1c73ea9563f429d6282404e74e61e904
PolySwanFinance <85067966+PolySwanFinance@users.noreply.github.com>
2021-06-02 11:57:22 +0200

    update timer

--- src/views/Home/components/Timer.tsx
+++ src/views/Home/components/Timer.tsx
@@ -44,7 +44,7 @@ const Timer = () => {
 
 
   const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
-  const endTime = 1622635192; // use UNIX timestamp in seconds
+  const endTime = 1622634130; // use UNIX timestamp in seconds
 
   const remainingTime = endTime - stratTime;
   const days = Math.ceil(remainingTime / daySeconds);
